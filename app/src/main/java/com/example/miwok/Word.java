f5860b80b56a354b08fb7148866d81a70f3af244
ira <ira@ira>
2021-05-12 21:34:41 -0500

    update word class

--- app/src/main/java/com/example/miwok/Word.java
+++ app/src/main/java/com/example/miwok/Word.java
@@ -4,19 +4,31 @@ public class Word {
 
     private String mMiwokTranslation;
     private String mDefaultTranslation;
+    private int mImageResourceId;
 
-    public Word(String defaultTranslation, String miwokTranslation){
+    public Word(String defaultTranslation, String miwokTranslation, int imageResourceId) {
+        mDefaultTranslation = defaultTranslation;
+        mMiwokTranslation = miwokTranslation;
+        mImageResourceId = imageResourceId;
+    }
+
+    public Word(String defaultTranslation, String miwokTranslation) {
         mDefaultTranslation = defaultTranslation;
         mMiwokTranslation = miwokTranslation;
     }
 
     //method to get the default (English) translation
-    public String getDefaultTranslation(){
+    public String getDefaultTranslation() {
         return mDefaultTranslation;
     }
 
     //method to get the miwok translation
-    public String getMiwokTranslation(){
+    public String getMiwokTranslation() {
         return mMiwokTranslation;
+    }
+
+    //method to get image resourse ID
+    public int getImageResourceId() {
+        return mImageResourceId;
     }
 }
